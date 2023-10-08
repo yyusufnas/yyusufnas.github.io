2b1212df2d42ddc4eda1e71c9d2588c238cc0a31
yyusufnas <yyusufnass@gmail.com>
2023-10-08 21:53:05 +0300

    slm

---
+++ egitimler.php
@@ -0,0 +1,63 @@
+<!DOCTYPE html>
+<html>
+<head>
+<style>
+  .container {
+    text-align: center;
+  }
+
+  .title {
+    font-size: 24px;
+    font-weight: bold;
+  }
+
+  .card {
+    display: inline-block;
+    margin: 10px;
+    width: 18rem;
+  }
+
+  .card img {
+    max-width: 100%;
+  }
+
+  .card p {
+    font-size: 16px;
+  }
+</style>
+</head>
+<body>
+
+<div class="container">
+  <h1 class="title">ANTALYA BOOTCAMP 2023</h1>
+  
+  <div class="card">
+    <img src="1.jpeg" class="card-img-top" alt="...">
+    <div class="card-body">
+      <p class="card-text">#siberyavuzlar</p>
+    </div>
+  </div>
+  <div class="card">
+    <img src="6.jpeg" class="card-img-top" alt="...">
+    <div class="card-body">
+      <p class="card-text">#siberyavuzlar</p>
+    </div>
+  </div>
+
+  <div class="card">
+    <img src="3.jpeg" class="card-img-top" alt="...">
+    <div class="card-body">
+      <p class="card-text">#siberyavuzlar</p>
+    </div>
+  </div>
+
+  <div class="card">
+    <img src="5.jpeg" class="card-img-top" alt="...">
+    <div class="card-body">
+      <p class="card-text">#siberyavuzlar</p>
+    </div>
+  </div>
+</div>
+
+</body>
+</html>
